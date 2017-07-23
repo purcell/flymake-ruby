6c320c6fb686c5223bf975cc35178ad6b195e073
Steve Purcell <steve@sanityinc.com>
2017-07-23 13:46:08 +1200

    Add licence info (GPL)
    Fixes https://github.com/purcell/flymake-coffee/issues/11

--- flymake-ruby.el
+++ flymake-ruby.el
@@ -1,11 +1,27 @@
 ;;; flymake-ruby.el --- A flymake handler for ruby-mode files
-;;
-;;; Author: Steve Purcell <steve@sanityinc.com>
-;;; URL: https://github.com/purcell/flymake-ruby
-;;; Version: DEV
-;;; Package-Requires: ((flymake-easy "0.1"))
-;;;
+
+;; Copyright (c) 2011-2017 Steve Purcell
+
+;; Author: Steve Purcell <steve@sanityinc.com>
+;; URL: https://github.com/purcell/flymake-ruby
+;; Package-Version: 0
+;; Package-Requires: ((flymake-easy "0.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
 ;; Usage:
 ;;   (require 'flymake-ruby)
 ;;   (add-hook 'ruby-mode-hook 'flymake-ruby-load)
@@ -24,7 +40,7 @@
 
 ;; Invoke ruby with '-c' to get syntax checking
 (defun flymake-ruby-command (filename)
-  "Construct a command that flymake can use to check ruby source."
+  "Construct a command that flymake can use to check ruby source in FILENAME."
   (list flymake-ruby-executable "-w" "-c" filename))
 
 ;;;###autoload
